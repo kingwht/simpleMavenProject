d4112554f88f597169cd7184fb8510af643fc974
kingwht <wht972332930@163.com>
2019-08-22 00:29:13 -0700

    fix path bugs

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -7,7 +7,4 @@ echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-TAG="${env.BUILD_ID}"
-echo $TAG
-
-cd jenkins/build && docker build -f Dockerfile-Java -t maven-project:"${env.BUILD_ID}" --no-cache .
+cd jenkins/build && docker build -f Dockerfile-Java -t maven-project:$BUILD_ID --no-cache .
