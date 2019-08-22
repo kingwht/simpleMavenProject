cb9a800514c62344d124a13499cc61db65921822
kingwht <wht972332930@163.com>
2019-08-21 17:33:33 -0700

    fix path bugs

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,6 +3,6 @@
 echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
-WORKSPACE=/home/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+WORKSPACE=~/jenkins-data/jenkins_home/workspace/simple-maven-project
 
 docker run --rm -v $WORKSPACE:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
