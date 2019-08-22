1841061e5f2b4bf8f19f1e6182af6d13cdc355e0
kingwht <wht972332930@163.com>
2019-08-21 18:10:09 -0700

    fix path bugs

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Copy the new jar to the build location
-cp -f java-app/target/*.jar jenkins/build/
+cp -f target/*.jar jenkins/build/
 
 echo "****************************"
 echo "** Building Docker Image ***"
